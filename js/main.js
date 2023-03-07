3260a4936d70b7d3fd09b8062bdd1b86c280eec5
Rudenches <xzdara@yandex.ru>
2023-03-07 22:36:49 +0300

    make Function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-let randomNumber = function (from, to) {
+const randomNumber = function (from, to) {
   if (from > to) {
     return null;
   }
-  if (from == to) {
+  if (from === to) {
     return to;
   }
   if (from >= 0 && to >=0) {
   for (let i = from; i <= to, i++;) {
 
-    let random = Math.floor(Math.random() * (to - from + 1)) + from ;
+    const random = Math.floor(Math.random() * (to - from + 1)) + from ;
 
     return random
   }
@@ -16,11 +16,14 @@ let randomNumber = function (from, to) {
 return null;
 }
 
-//console.log(randomNumber(3,3));
+console.log(randomNumber(3,3));
 
-let isStringMax = function (str, maxValue) {
-  if (str.length <= maxValue )
+const isStringMax = function (str, maxValue) {
+  if (str.length <= maxValue ) {
   return true
-  else  return false
+  }
+  else {
+    return false
+  }
 }
-//console.log(isStringMax('как дела', 10));
+console.log(isStringMax('как дела', 10));
