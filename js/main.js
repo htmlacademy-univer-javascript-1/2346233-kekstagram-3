e2061978bc326e11a063692947f41876de49d33e
Rudenches <xzdara@yandex.ru>
2023-03-07 22:26:27 +0300

    make function

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let randomNumber = function (from, to) {
+  if (from > to) {
+    return null;
+  }
+  if (from == to) {
+    return to;
+  }
+  if (from >= 0 && to >=0) {
+  for (let i = from; i <= to, i++;) {
+
+    let random = Math.floor(Math.random() * (to - from + 1)) + from ;
+
+    return random
+  }
+}
+return null;
+}
+
+//console.log(randomNumber(3,3));
+
+let isStringMax = function (str, maxValue) {
+  if (str.length <= maxValue )
+  return true
+  else  return false
+}
+//console.log(isStringMax('как дела', 10));
