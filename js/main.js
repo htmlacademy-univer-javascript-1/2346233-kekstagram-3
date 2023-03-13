eb548cc26975f97c7739a8bd1620dac0c4d8dfea
Rudenches <xzdara@yandex.ru>
2023-03-13 17:10:37 +0300

    new

--- js/main.js
+++ js/main.js
@@ -5,19 +5,39 @@ const getRandomNumber =  (from, to) => {
   if (from === to) {
     return to;
   }
-  if (from >= 0 && to >=0) {
-    for (let i = from; i <= to, i++;) {
+  if (from >=0 && to >=0) {
+    for (let i = from; i < to, i+=1;) {
+      from = Math.ceil(from);
+      to = Math.floor(to);
 
       const random = Math.floor(Math.random() * (to - from + 1)) + from ;
 
       return random;
     }
+  } else {
+    throw new Error('negative number');
   }
-  throw new Error('negative number');
 };
 
-getRandomNumber(3,3);
+//console.log(getRandomNumber(0,3));
 
 const isStringMax = (str, maxValue) =>  str.length <= maxValue;
 
 isStringMax('как дела', 10);
+
+const MAX_PHOTOS = 25;
+
+const descriptions = ['зимнее утро', 'утро в саду', 'морской бриз', 'завтрак', 'Ночная тьма'];
+
+const getPhotos = () => ({
+  id: getRandomNumber(1,MAX_PHOTOS),
+  url: `photos/${getRandomNumber(1,MAX_PHOTOS)}.jpg`,
+  description: descriptions[getRandomNumber(0,4)],
+  likes: getRandomNumber(15,200),
+  comments: getRandomNumber(0,200),
+});
+
+// eslint-disable-next-line no-unused-vars
+const massiveOfPhotos = Array.from({length:MAX_PHOTOS}, getPhotos);
+
+
