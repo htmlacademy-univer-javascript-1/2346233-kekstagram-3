a0360c7d428b517e972da8a8e603f87a71679b79
Rudenches <xzdara@yandex.ru>
2023-05-02 16:01:47 +0300

    module 8

--- js/drawPhotos.js
+++ js/drawPhotos.js
@@ -1,21 +1,17 @@
 
 const pictureTemplate = document.querySelector('#picture');
-const pictures = document.querySelector('.pictures');
-const fragment = document.createDocumentFragment();
+const pictureContainer = document.querySelector('.pictures');
 
 
-export const  insertPhotoMiniature = (photos) => {
-  photos.forEach((photo) => {
-    const template = pictureTemplate.cloneNode(true);
-    const img = template.querySelector('.picture__img');
-    const likes = template.querySelector('.pictures__likes');
-    const comments = template.querySelector('.picture__comments');
-    img.src = photo.url;
-    likes.textContent = photo.likes;
-    comments.textContent = photo.comment;
-
-    fragment.appendChild(template);
-  });
-  pictures.append(fragment);
-};
+export function insertPhotoMiniature(photos) {
+  const pictureFragment = document.createDocumentFragment();
+  for (const {url, likes, comments} of photos) {
+    const picture = pictureTemplate.cloneNode(true).content;
+    picture.querySelector('.picture__img').src = url;
+    picture.querySelector('.picture__likes').textContent = likes;
+    picture.querySelector('.picture__comments').textContent = comments.length;
+    pictureFragment.appendChild(picture);
+  }
+  pictureContainer.appendChild(pictureFragment);
+}
 
